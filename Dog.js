6cc9f63fdd17f5ad064e39d5000a9d54d89a73f0
Your Name <you@example.com>
2021-03-03 16:42:41 +0700

    add methold for dog

--- Dog.js
+++ Dog.js
@@ -1,3 +1,9 @@
-function Dog() {
-	// body...
+var chalk= require("chalk");
+function Dog(name) {
+	this.name= name;
+	this.stomach=[];
 }
+Dog.prototype.sayHi = function() {
+	console.log("Hi! I am a dog. My name is "+chalk.red(this.name));
+};
+module.exports=Dog;
