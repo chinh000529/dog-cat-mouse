b3627cdd57650fd76e8ff2ba3af547a47ac6bf06
Your Name <you@example.com>
2021-03-03 17:03:55 +0700

    add method

--- cat.js
+++ cat.js
@@ -1,4 +1,7 @@
-function Cat() {
-	
+function Cat(name) {
+	this.name= name;
 }
+Cat.prototype.sayHi = function() {
+	console.log("Hi! I am a cat, my name is " +this.name);
+};
 module.exports= Cat;
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var Dog= require("./Dog");
+var Cat= require("./cat");
+
 var dog= new Dog("Tom");
 dog.sayHi();
+
+var cat= new Cat("Meo");
+cat.sayHi();
